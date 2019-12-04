9c98f35b5006110f25080faf5e9c34ba12a342d1
Stephane-Jean <stephanejean.martini@gmail.com>
2019-12-04 10:06:30 +0100

    Compute the median for each season

--- pandasAnalysis.py
+++ pandasAnalysis.py
@@ -5,7 +5,6 @@ import math
 filename = "OpenDataEcoCO2.csv"
 dfCO2 = pd.read_csv(filename)
 
-
 # Interpolate a measure every quarter of an hour instead of every half hour
 dfCO2Inter = dfCO2.interpolate(method ='linear', limit_direction ='forward')
 
@@ -14,11 +13,30 @@ dfCO2Inter = dfCO2.interpolate(method ='linear', limit_direction ='forward')
 dfCO2['WeekDay'] = pd.DatetimeIndex(dfCO2['Date']).weekday
 dfCO2Inter['WeekDay'] = pd.DatetimeIndex(dfCO2Inter['Date']).weekday
 
-
 # loc allow to select only the rows that corresponds to a weekend
-print(dfCO2Inter.loc[(dfCO2Inter['WeekDay']) >= 5]['Taux de CO2 (g/kWh)'].mean(axis=0))
-print(dfCO2.loc[(dfCO2['WeekDay']) >= 5]['Taux de CO2 (g/kWh)'].mean(axis=0, skipna=True))
+print("Weekend Interpolated Mean :"+str(dfCO2Inter.loc[(dfCO2Inter['WeekDay']) >= 5]['Taux de CO2 (g/kWh)'].mean(axis=0)))
+print("Weekend Mean :"+str(dfCO2.loc[(dfCO2['WeekDay']) >= 5]['Taux de CO2 (g/kWh)'].mean(axis=0, skipna=True)))
 
 # loc allow to select only the rows that corresponds to a day of the week
-print(dfCO2Inter.loc[(dfCO2Inter['WeekDay']) <= 4]['Taux de CO2 (g/kWh)'].mean(axis=0))
-print(dfCO2.loc[(dfCO2['WeekDay']) <= 4]['Taux de CO2 (g/kWh)'].mean(axis=0, skipna=True))
+print("Weekday Interpolated Mean :"+str(dfCO2Inter.loc[(dfCO2Inter['WeekDay']) <= 4]['Taux de CO2 (g/kWh)'].mean(axis=0)))
+print("Weekday Mean :"+str(dfCO2.loc[(dfCO2['WeekDay']) <= 4]['Taux de CO2 (g/kWh)'].mean(axis=0, skipna=True)))
+
+
+# Median for each of the 8 seasons of teh dataset
+print("Winter 2017 Interpolated Median :"+str(dfCO2Inter.loc[((dfCO2Inter['Date']) >= '2017-01-01') & ((dfCO2Inter['Date']) <= '2017-03-20')]['Taux de CO2 (g/kWh)'].median(axis=0)))
+print("Spring 2017 Interpolated Median :"+str(dfCO2Inter.loc[((dfCO2Inter['Date']) >= '2017-03-21') & ((dfCO2Inter['Date']) <= '2017-06-20')]['Taux de CO2 (g/kWh)'].median(axis=0)))
+print("Summer 2017 Interpolated Median :"+str(dfCO2Inter.loc[((dfCO2Inter['Date']) >= '2017-06-21') & ((dfCO2Inter['Date']) <= '2017-09-20')]['Taux de CO2 (g/kWh)'].median(axis=0)))
+print("Fall 2017 Interpolated Median :"+str(dfCO2Inter.loc[((dfCO2Inter['Date']) >= '2017-09-21') & ((dfCO2Inter['Date']) <= '2017-12-20')]['Taux de CO2 (g/kWh)'].median(axis=0)))
+print("Winter 2018 Interpolated Median :"+str(dfCO2Inter.loc[((dfCO2Inter['Date']) >= '2017-12-21') & ((dfCO2Inter['Date']) <= '2018-03-20')]['Taux de CO2 (g/kWh)'].median(axis=0)))
+print("Spring 2018 Interpolated Median :"+str(dfCO2Inter.loc[((dfCO2Inter['Date']) >= '2018-03-21') & ((dfCO2Inter['Date']) <= '2018-06-20')]['Taux de CO2 (g/kWh)'].median(axis=0)))
+print("Summer 2018 Interpolated Median :"+str(dfCO2Inter.loc[((dfCO2Inter['Date']) >= '2018-06-21') & ((dfCO2Inter['Date']) <= '2018-09-20')]['Taux de CO2 (g/kWh)'].median(axis=0)))
+print("Fall 2018 Interpolated Median :"+str(dfCO2Inter.loc[((dfCO2Inter['Date']) >= '2018-09-21') & ((dfCO2Inter['Date']) <= '2018-12-20')]['Taux de CO2 (g/kWh)'].median(axis=0)))
+
+print("Winter 2017 Median :"+str(dfCO2.loc[((dfCO2['Date']) >= '2017-01-01') & ((dfCO2['Date']) <= '2017-03-20')]['Taux de CO2 (g/kWh)'].median(axis=0, skipna=True)))
+print("Spring 2017 Median :"+str(dfCO2.loc[((dfCO2['Date']) >= '2017-03-21') & ((dfCO2['Date']) <= '2017-06-20')]['Taux de CO2 (g/kWh)'].median(axis=0, skipna=True)))
+print("Summer 2017 Median :"+str(dfCO2.loc[((dfCO2['Date']) >= '2017-06-21') & ((dfCO2['Date']) <= '2017-09-20')]['Taux de CO2 (g/kWh)'].median(axis=0, skipna=True)))
+print("Fall 2017 Median :"+str(dfCO2.loc[((dfCO2['Date']) >= '2017-09-21') & ((dfCO2['Date']) <= '2017-12-20')]['Taux de CO2 (g/kWh)'].median(axis=0, skipna=True)))
+print("Winter 2018 Median :"+str(dfCO2.loc[((dfCO2['Date']) >= '2017-12-21') & ((dfCO2['Date']) <= '2018-03-20')]['Taux de CO2 (g/kWh)'].median(axis=0, skipna=True)))
+print("Spring 2018 Median :"+str(dfCO2.loc[((dfCO2['Date']) >= '2018-03-21') & ((dfCO2['Date']) <= '2018-06-20')]['Taux de CO2 (g/kWh)'].median(axis=0, skipna=True)))
+print("Summer 2018 Median :"+str(dfCO2.loc[((dfCO2['Date']) >= '2018-06-21') & ((dfCO2['Date']) <= '2018-09-20')]['Taux de CO2 (g/kWh)'].median(axis=0, skipna=True)))
+print("Fall 2018 Median :"+str(dfCO2.loc[((dfCO2['Date']) >= '2018-09-21') & ((dfCO2['Date']) <= '2018-12-20')]['Taux de CO2 (g/kWh)'].median(axis=0, skipna=True)))
